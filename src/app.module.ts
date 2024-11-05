d945a28172c6ca52e4e0002a7b4ed31bb0b27c50
Toto-Sempai <totochucl@gmail.com>
2024-11-05 01:56:24 -0500

    config basic and base de datod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        console.log('Conectando a la base de datos con las siguientes configuraciones:');
+        console.log({
+          host: process.env.DB_HOST,
+          port: +process.env.DB_PORT,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+        });
+        return {
+          type: 'mysql',
+          host: process.env.DB_HOST,
+          port: +process.env.DB_PORT,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
