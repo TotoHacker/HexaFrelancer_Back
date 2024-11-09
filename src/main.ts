3f857dfbc2d275b96cde3629bd7df3ca07202620
MaximoQuin <169080723+MaximoQuin@users.noreply.github.com>
2024-11-09 09:30:59 -0600

    modificando la conexion y configurando el puerto 8080 y creando el crud user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SERVER_PORT } from './config/constans';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>(SERVER_PORT) || 3000;
+  await app.listen(port);
+  console.log("Escuchando en el puerto ", await app.getUrl());
+  
 }
 bootstrap();
