4ce590419b0aae2d2f904c0029216bfee7583dbf
Toto-Sempai <totochucl@gmail.com>
2024-11-12 19:12:30 -0500

    Configuracion de capas

--- src/CapaAplicacion/app.module.ts
+++ src/CapaAplicacion/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UserModule } from './user/user.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
