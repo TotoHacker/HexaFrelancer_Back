e6723fb83986e846dcd28c9bc054a4a7218d1581
MaximoQuin <169080723+MaximoQuin@users.noreply.github.com>
2024-11-12 18:39:42 -0600

    corretion

--- src/CapaAplicacion/app.module.ts
+++ src/CapaAplicacion/app.module.ts
@@ -26,29 +26,7 @@ import { UserModule } from '../user/user.module';
       }),
       inject: [ConfigService],
     }),
-
     UserModule,
-    // TypeOrmModule.forRootAsync({
-    //   useFactory: () => {
-    //     console.log('Conectando a la base de datos con las siguientes configuraciones:');
-    //     console.log({
-    //       host: process.env.DB_HOST,
-    //       port: +process.env.DB_PORT,
-    //       username: process.env.DB_USERNAME,
-    //       password: process.env.DB_PASSWORD,
-    //       database: process.env.DB_NAME,
-    //     });
-    //     return {
-    //       type: 'mysql',
-    //       host: process.env.DB_HOST,
-    //       port: +process.env.DB_PORT,
-    //       username: process.env.DB_USERNAME,
-    //       password: process.env.DB_PASSWORD,
-    //       database: process.env.DB_NAME,
-    //       synchronize: true,
-    //     };
-    //   },
-    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
